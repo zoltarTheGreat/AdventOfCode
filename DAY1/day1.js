9f507fbef9c33c1b6082d3b85a10ca78bdfd50b1
ZoltarTheGreat <cristianccabrera@gmail.com>
2020-12-04 13:24:03 -0500

    Day 1 complete. 2 EZ

--- DAY1/day1.js
+++ DAY1/day1.js
@@ -7,39 +7,70 @@ function loadFile() {
 }
 
 loadFile();
-numbers.sort(function (a, b) {  return a - b;  });
+numbers.sort(function (a, b) { return a - b; });
 
-for(i = 0; i < numbers.length; i++)
-{
+for (i = 0; i < numbers.length; i++) {
     var currNum = numbers[i];
     var solved = false;
-    if( currNum == 0)
-    {
+    if (currNum == 0) {
         continue;
     }
 
-    for(j = numbers.length; j > i; --j)
-    {
-        if(numbers[i] + numbers[j] > 2020)
-        {
+    for (j = numbers.length - 1; j > i; j--) {
+        if (numbers[i] + numbers[j] > 2020) {
             continue;
         }
-        else if(numbers[i] + numbers[j] == 2020)
-        {
+        else if (numbers[i] + numbers[j] == 2020) {
             solved = true;
             console.log(numbers[i] + numbers[j]);
             console.log(numbers[i] * numbers[j]);
             break;
         }
-        else if(numbers[i] + numbers[j] < 2020)
-        {
+        else if (numbers[i] + numbers[j] < 2020) {
             break;
         }
     }
 
-    if(solved)
-    {
+    if (solved) {
         console.log("WOO");
         break;
     }
 }
+
+for (i = 0; i < numbers.length; i++) {
+    var currNum = numbers[i];
+    var solved = false;
+    if (currNum == 0) {
+        continue;
+    }
+
+    for (j = i + 1; j < numbers.length; j++) {
+
+        for (k = j + 1; k < numbers.length; k++) {
+
+            if (numbers[i] + numbers[j] + numbers[k] < 2020) {
+                continue;
+            }
+            else if (numbers[i] + numbers[j] + numbers[k] == 2020) {
+                solved = true;
+                console.log(numbers[i] + numbers[j] + numbers[k]);
+                console.log(numbers[i] * numbers[j] * numbers[k]);
+                break;
+            }
+            else if (numbers[i] + numbers[j] + numbers[k] > 2020) {
+                break;
+            }
+        }
+        
+        if (solved) {
+            console.log("WOO");
+            break;
+        }
+    }
+
+    if (solved) {
+        console.log("WOO");
+        break;
+    }
+}
+
