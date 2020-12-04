858a8de4ecd126941dd64addd8b224b4302d945e
ZoltarTheGreat <cristianccabrera@gmail.com>
2020-12-04 12:57:08 -0500

    Initial Commit. I did this in js for the memes.

---
+++ DAY1/day1.js
@@ -0,0 +1,45 @@
+var numbers = new Int8Array();
+
+function loadFile() {
+    var fs = require("fs");
+    var text = fs.readFileSync("input.txt").toString('utf-8');
+    numbers = text.split("\n").map(Number);
+}
+
+loadFile();
+numbers.sort(function (a, b) {  return a - b;  });
+
+for(i = 0; i < numbers.length; i++)
+{
+    var currNum = numbers[i];
+    var solved = false;
+    if( currNum == 0)
+    {
+        continue;
+    }
+
+    for(j = numbers.length; j > i; --j)
+    {
+        if(numbers[i] + numbers[j] > 2020)
+        {
+            continue;
+        }
+        else if(numbers[i] + numbers[j] == 2020)
+        {
+            solved = true;
+            console.log(numbers[i] + numbers[j]);
+            console.log(numbers[i] * numbers[j]);
+            break;
+        }
+        else if(numbers[i] + numbers[j] < 2020)
+        {
+            break;
+        }
+    }
+
+    if(solved)
+    {
+        console.log("WOO");
+        break;
+    }
+}
